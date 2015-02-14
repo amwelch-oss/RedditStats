88585cdb1a79deb5456d6e7e87bfe862cf77df42
Sandy <amwelch@umich.edu>
2015-02-14 13:33:14 -0500

    basic setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='reditstats',
+      version='1.0',
+      description='Statistics and Analytics for Redit',
+      author='Alexander Welch',
+      author_email='amwelch3@gmail.com',
+      packages=['reditstats'],
+     )
