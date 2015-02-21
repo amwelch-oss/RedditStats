505d5314a7d1c1d6f5ad359979ff3a71c47cfdc6
Sandy <amwelch@umich.edu>
2015-02-20 21:24:50 -0500

    missed matplotlib

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ from setuptools import setup
 setup(name='redditstats',
       version='1.0',
       description='Statistics and Analytics for Redit',
-      author='Alexander Welch',
+      author='Alexaeder Welch',
       author_email='amwelch3@gmail.com',
-      install_requires=['praw', 'arrow', 'pandas', 'numpy'],
+      install_requires=['praw', 'arrow', 'pandas', 'numpy', 'matplotlib'],
       test_requires=['unittest', 'mock'],
       packages=['redditstats'],
      )
