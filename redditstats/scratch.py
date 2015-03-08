dc097daef377e21823a1c7f143b66f911e7cadd7
Sandy <amwelch@umich.edu>
2015-03-07 20:29:53 -0500

    remove ipdb from scratch

--- redditstats/scratch.py
+++ redditstats/scratch.py
@@ -21,14 +21,10 @@ def main():
     args = parse_args()
 
     conn = connect.create_connection()
-    import ipdb
-    ipdb.set_trace()
 
     df = subreddit.get_posts_summary(conn, 'soccer', limit=10)
  
     print df
-    import ipdb
-    ipdb.set_trace()
     stats.generate_cdf(df)
 
     return
